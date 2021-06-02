c8fa1122665ef8cbe5fc58e55f87c6ba22cf2864
PitzTech <victorlaurentino7@gmail.com>
2021-06-02 12:12:46 -0300

    Project CRUD Complete

--- desafio-conceitos-nodejs/src/app.js
+++ desafio-conceitos-nodejs/src/app.js
@@ -1,33 +1,81 @@
 const express = require("express");
 const cors = require("cors");
-
+const { v4, validate } = require("uuid")
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/repositories/:id", validadeRepId)
 
 const repositories = [];
 
+function validadeRepId(request, response, next) {
+    const { id } = request.params
+
+    if(!validate(id))
+        return response.status(400).json({ error: "Invalid Repository ID."})
+
+    return next()
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body
+
+    const repository = {
+        id: v4(),
+        title,
+        url,
+        techs,
+        likes: 0
+    }
+
+    repositories.push(repository)
+
+    return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params
+    const { title, url, techs } = request.body
+
+    const repIndex = repositories.findIndex(rep => rep.id == id)
+
+    const repository = {
+        ...repositories[repIndex],
+        title,
+        url,
+        techs
+    }
+
+    repositories[repIndex] = repository
+
+    return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params
+    
+    const repIndex = repositories.findIndex(rep => rep.id == id)
+
+    repositories.splice(repIndex, 1)
+
+    return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params
+
+    const repository = repositories.find(rep => rep.id == id)
+
+    repository.likes += 1
+
+    return response.json(repository)
 });
 
 module.exports = app;
